f0e6aacc012c982f5041bf301a8cefa93a973f2e
Emir <e.navruzov@ktsstudio.ru>
2019-10-19 13:23:49 +0300

    books count 10

--- api/views.py
+++ api/views.py
@@ -18,7 +18,8 @@ async def get_book(request, book_isbn):
 
 
 async def list_random_books(request):
+    books_count = 10
     books_isbn = list(request.app['books_dict'])
-    random_indexes = random.sample(range(len(books_isbn)), 15)
+    random_indexes = random.sample(range(len(books_isbn)), books_count)
     random_isbn = [books_isbn[i] for i in random_indexes]
     return json_response({'books': await ListBooksISBNTask(random_isbn).main()})
